aebc8f5172d80a1bbf168c5bd04dd6483c587c7f
Renan G Dutra <r.granatodutra@gmail.com>
2024-07-02 13:56:27 -0300

    build

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vb0T/iMXP2WEIASexI7BqgKqtaURbVUPK4kcQ5mlM1I=\"}"
--- src/app/lib/instances.service.ts
+++ src/app/lib/instances.service.ts
@@ -1,5 +1,5 @@
 import { InstancesMannager } from "inpulse-crm/connection/src/instances-mannager";
 
-const instancesService = new InstancesMannager(process.env.REACT_APP_API_URL || "http://localhost:9000");
+const instancesService = new InstancesMannager(process.env.REACT_APP_API_URL || "https://contec.inf.br");
 
 export default instancesService;
